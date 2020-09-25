5e47355ba21026afb6ae766231b1292990e2ef5b
PratikGarai <pratikgarai0208@gmail.com>
2020-09-25 20:15:26 +0530

    Added some more stuff

---
+++ AirlineReservation/User/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'User'
+
+urlpatterns = [
+        path('', views.landing)
+        ]
--- AirlineReservation/User/views.py
+++ AirlineReservation/User/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    
+    if not request.user.is_authenticated:
+        print("Not logged in")
+    else :
+        print("logged in")
+    return render(request, 'MessagePage.html', {"title" : "Sample Title", "message" : "The sample page"})
+
+def register(request):
+
+    if request.user.is_authenticated:
+        return render(request, "MessagePage.html", {"title": "Error", "message": "You are already logged in!"})
