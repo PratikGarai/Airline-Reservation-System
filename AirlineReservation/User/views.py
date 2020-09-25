79cd3845f3ff751a2389eb1591d5236c5414de71
PratikGarai <pratikgarai0208@gmail.com>
2020-09-25 20:24:26 +0530

    Added the registration views

--- AirlineReservation/User/views.py
+++ AirlineReservation/User/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from forms import UserForm
+from models import Passenger
 
 def landing(request):
     
@@ -12,3 +14,21 @@ def register(request):
 
     if request.user.is_authenticated:
         return render(request, "MessagePage.html", {"title": "Error", "message": "You are already logged in!"})
+
+    if request.method=="POST":
+        user_form = forms.UserForm(data = request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            p = Passenger()
+            Passenger.user = user
+            Passenger.save()
+             
+            return render(request, "MessagePage.html", {"title": "Success", "message": "You have successfully registered!"})
+
+        else:
+            return render(request, "RegistrationForm.html", { "form":UserForm, "error":True, "error_msg":"Form Invalid!" })
+
+
+        return render(request, "RegistrationForm.html", { "form":UserForm, "error":False, "error_msg":"" })
