fd063db16d013da2dddd0e8fa9ab415d61b01fb6
PratikGarai <pratikgarai0208@gmail.com>
2020-09-28 10:26:12 +0530

    Added some part to the models

--- AirlineReservation/AirApp/models.py
+++ AirlineReservation/AirApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from User.models import Passenger
 
-# Create your models here.
+locations = [
+        ( 'IND', 'India' ),
+        ( 'PAK', 'Pakistan' ),
+        ( 'USA', 'United States'),
+        ( 'GER', 'Germany'),
+        ( 'FRA', 'France')
+    ]
+
+
+class Flight(models.Model):
+
+    number = models.CharField(max_length=10, blank = False)
+    source = models.CharField(max_length=10, choices = locations)
+    destination = models.CharField(max_length=10, choices = locations)
+    departure = models.DateTimeField()
+    duration = models.DurationField()
+
+class Ticket(models.Model):
+
+    passenger = models.ForeignKey(Passenger, realted_name='tickets', on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, related_name='bookings',on_delete=models.CASCADE)
+    number = models.CharField(max_length=10, blank = False)
+    source = models.CharField(max_length=10, choices = locations)
+    destination = models.CharField(max_length=10, choices = locations)
+    booked_at = models.DateTimeField(auto_now = True)
+
