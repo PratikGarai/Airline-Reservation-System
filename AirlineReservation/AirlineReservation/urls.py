e922155602201ec82ad8b15a108fe76594268229
PratikGarai <pratikgarai0208@gmail.com>
2020-09-29 23:56:53 +0530

    Navigation works fine

--- AirlineReservation/AirlineReservation/urls.py
+++ AirlineReservation/AirlineReservation/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('User.urls'), name="Account"),
+    path('accounts/', include('User.urls'), name="User"),
     path('', include('AirApp.urls'), name="App")
 ]
