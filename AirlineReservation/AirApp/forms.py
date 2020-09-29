10856730721fa90b9d263a46a41b29da5d8696f4
PratikGarai <pratikgarai0208@gmail.com>
2020-09-30 02:11:13 +0530

    Main page form basics made

--- AirlineReservation/AirApp/forms.py
+++ AirlineReservation/AirApp/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from . import models
 
+class FlightFilterForm(forms.Form):
+    source = forms.CharField(widget = forms.Select(choices = models.locations))
+    destination = forms.CharField(widget = forms.Select(choices = models.locations))
+    date = forms.DateField()
+
 class TicketForm(forms.ModelForm):
     class Meta :
         model = models.Ticket
--- AirlineReservation/AirApp/views.py
+++ AirlineReservation/AirApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from . import forms
 from . import models
 
 def landing(request):
-    return render(request, "MessagePage.html", {"title" : "Landing", "message":"This is the landing page"});
+    return render(request, "FormPage.html", {"title":"Welcome!", "form":forms.FlightFilterForm, "error":False, "error_msg":""})
 
+@login_required
 def bookticket(request, flight_id):
     return render(request, "FormPage.html", {"title":"Booking", "form":forms.TicketForm, "error":True, "error_msg":"Flight id : "+str(flight_id)})
