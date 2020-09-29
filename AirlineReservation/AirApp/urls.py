c6d763574a8d329675703eb8a06107b2d5afe469
PratikGarai <pratikgarai0208@gmail.com>
2020-09-30 03:04:14 +0530

    Added the flights display page

--- AirlineReservation/AirApp/urls.py
+++ AirlineReservation/AirApp/urls.py
@@ -5,5 +5,6 @@ app_name = "App"
 
 urlpatterns = [
         path('', views.landing, name="Landing"),
+        path('flights/', views.flight_full_list, name="FullList"),
         path('book/<int:flight_id>', views.bookticket, name="Booking"),
 ]
--- AirlineReservation/AirApp/views.py
+++ AirlineReservation/AirApp/views.py
@@ -6,6 +6,9 @@ from . import models
 def landing(request):
     return render(request, "FormPage.html", {"title":"Welcome!", "form":forms.FlightFilterForm, "error":False, "error_msg":""})
 
+def flight_full_list(request):
+    return render(request, "FlightList.html", { "flight_list": models.Flight.objects.all() })
+
 @login_required
 def bookticket(request, flight_id):
     return render(request, "FormPage.html", {"title":"Booking", "form":forms.TicketForm, "error":True, "error_msg":"Flight id : "+str(flight_id)})
