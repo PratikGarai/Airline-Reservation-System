0f38edb996adcada1a84fc1b92b0247464090c62
PratikGarai <pratikgarai0208@gmail.com>
2020-09-30 11:29:58 +0530

    configure static files

--- AirlineReservation/AirlineReservation/urls.py
+++ AirlineReservation/AirlineReservation/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path('accounts/', include('User.urls'), name="User"),
     path('', include('AirApp.urls'), name="App")
 ]
+
+from . import settings
+from django.contrib.staticfiles.urls import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
