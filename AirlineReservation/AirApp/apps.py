0c7184de6095ca07ad304085494a510b405becd3
PratikGarai <pratikgarai0208@gmail.com>
2020-12-11 17:15:05 +0530

    Removed old codes

--- AirlineReservation/AirApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirappConfig(AppConfig):
-    name = 'AirApp'
--- README.md
+++
@@ -1,2 +0,0 @@
-# Airline-Reservation-System
-A simple airline resevation system using Django Framework
